dd5fc54dab7125101961f52d6ab5925386679ac0
Jesus Sanchez <jhsanro@gmail.com>
2025-12-16 17:58:52 -0500

    Update Firebase configuration to use environment variables: Replaced hardcoded Firebase credentials with references to environment variables for improved security and flexibility in configuration management.

--- src/infrastructure/firebase/config.ts
+++ src/infrastructure/firebase/config.ts
@@ -6,13 +6,13 @@ import { getFirestore, Firestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA0Uodm14Z5rp2xo4QCULr5dwf7RngaWuc",
-  authDomain: "connect-tierra.firebaseapp.com",
-  projectId: "connect-tierra",
-  storageBucket: "connect-tierra.firebasestorage.app",
-  messagingSenderId: "154649592138",
-  appId: "1:154649592138:web:400f05f7287012e49741b3",
-  measurementId: "G-Z7V9RRVVL1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
